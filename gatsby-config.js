8886a50f0e1a4f8dbf6343524d8af444b54f388c
vally <vally.pepyako@gmail.com>
2019-11-28 17:20:55 +0300

    add gatsby-transformer-remark

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,14 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `md`,
+        path: `${__dirname}/data`,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
