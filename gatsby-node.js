7a3f785bf5a9b5f8f8e7b518d8445bbcebf31c7c
vally <vally.pepyako@gmail.com>
2019-11-28 17:53:35 +0300

    add blog posts generator

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,30 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-
-// You can delete this file if you're not using it
+const path = require(`path`)
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  const { createPage } = actions
+  const blogPost = path.resolve(`src/templates/blogPost.js`)
+  const result = await graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              path
+            }
+          }
+        }
+      }
+    }
+  `)
+  // Handle errors
+  if (result.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`)
+    return
+  }
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: node.frontmatter.path,
+      component: blogPost,
+      context: {}, // additional data can be passed via context
+    })
+  })
+}
