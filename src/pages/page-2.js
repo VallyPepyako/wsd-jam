f3551ab21246f2fd647c072b73ff616daa2f0c04
vally <vally.pepyako@gmail.com>
2019-11-28 18:21:44 +0300

    add blog navigation to page 2

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,16 +1,34 @@
 import React from "react"
-import { Link } from "gatsby"
-
+import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
-  <Layout>
+const SecondPage = ({data}) => (
+  console.log(data.allMarkdownRemark.edges) || <Layout>
     <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <h1>Война и мир(в основном война)</h1>
     <Link to="/">Go back to the homepage</Link>
+    <ul>
+      {data.allMarkdownRemark.edges.map(item => (
+        <li key={item.node.frontmatter.path}><Link to={item.node.frontmatter.path}>{item.node.frontmatter.title}</Link></li>
+      ))}
+    </ul>
   </Layout>
 )
+
+export const query = graphql`
+query HomePageQuery {
+    allMarkdownRemark(sort: {fields: frontmatter___path}) {
+      edges {
+        node {
+          frontmatter {
+            path
+            title
+          }
+        }
+      }
+    }
+  }
+`
 
 export default SecondPage
